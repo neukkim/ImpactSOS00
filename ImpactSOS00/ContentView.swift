ab6961db3ef1fd3738d632ea7a5163e02209afc7
sunminhong <akoda5555@gmail.com>
2025-05-07 20:47:22 +0900

    Initial Commit

---
+++ ImpactSOS00/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImpactSOS00
+//
+//  Created by SunMin Hong on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImpactSOS00/ImpactSOS00App.swift
@@ -0,0 +1,17 @@
+//
+//  ImpactSOS00App.swift
+//  ImpactSOS00
+//
+//  Created by SunMin Hong on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ImpactSOS00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
